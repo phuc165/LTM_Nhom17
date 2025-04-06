4b137382af4a78dd7a4d8c77a2a13cb6664b2ccf
phuc165 <hatanphuc3456789@gmail.com>
2025-04-06 09:32:57 +0700

    add server ssl certificate

--- CaroServer/Server.cs
+++ CaroServer/Server.cs
@@ -66,7 +66,7 @@ namespace CaroServer
             this.updateStatusCallback = updateStatusCallback;
 
             string certPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\server.pfx");
-            string certPassword = "Kiet.132003";
+            string certPassword = "nhom17";
             serverCertificate = new X509Certificate2(certPath, certPassword);
         }
 
